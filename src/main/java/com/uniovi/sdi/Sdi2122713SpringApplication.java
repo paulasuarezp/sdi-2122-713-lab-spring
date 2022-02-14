c01332a392119487f86f49614b7cdec505703b25
uo269745 <71699020+uo269745@users.noreply.github.com>
2022-02-14 11:29:19 +0100

    SDI-2122-713-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi/Sdi2122713SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2122713SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2122713SpringApplication.class, args);
+    }
+
+}
