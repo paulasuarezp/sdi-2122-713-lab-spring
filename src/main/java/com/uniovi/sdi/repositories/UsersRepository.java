a7c787cbce6c1b578edc3981ae11ffcdef34ffe8
Paula Suarez Prieto - UO269745 <uo269745@uniovi.es>
2022-02-23 00:47:50 +0100

    SDI-2122-713-3.3-Acceso a los datos con entidades relacionadas

---
+++ src/main/java/com/uniovi/sdi/repositories/UsersRepository.java
@@ -0,0 +1,6 @@
+package com.uniovi.sdi.repositories;
+
+import com.uniovi.sdi.entities.*;
+import org.springframework.data.repository.CrudRepository;
+public interface UsersRepository extends CrudRepository<User, Long>{
+}
