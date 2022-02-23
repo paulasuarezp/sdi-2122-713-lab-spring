af9f32914e3dc0e49b86bc19d860258671baf340
Paula Suarez Prieto - UO269745 <uo269745@uniovi.es>
2022-02-23 08:39:28 +0100

    SDI-2122-713-3.5-Validación de datos en el servidor

--- src/main/java/com/uniovi/sdi/controllers/UsersController.java
+++ src/main/java/com/uniovi/sdi/controllers/UsersController.java
@@ -7,8 +7,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.sdi.entities.*;
+import com.uniovi.sdi.validators.SignUpFormValidator;
 
 
 
@@ -19,6 +22,9 @@ public class UsersController {
 
     @Autowired
     private SecurityService securityService;
+
+    @Autowired
+    private SignUpFormValidator signUpFormValidator;
 
     @RequestMapping("/user/list")
     public String getListado(Model model) {
@@ -58,11 +64,23 @@ public class UsersController {
     }
 
     @RequestMapping(value = "/signup", method = {RequestMethod.POST})
-    public String signup(@ModelAttribute("user") User user, Model model) {
+    public String signup(@Validated User user, BindingResult result) {
+        signUpFormValidator.validate(user,result);
+        if(result.hasErrors()){
+            return "signup";
+        }
+
         usersService.addUser(user);
         securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
         return "redirect:home";
     }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Model model) {
         return "login";
